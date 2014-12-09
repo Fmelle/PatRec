36f94ab169c13f9e3d25522e89550c337e6946bd
Spencer Barton <sbarton272@gmail.com>
2014-12-08 20:09:27 -0500

    Working on tests

--- Code/Validation/testScript.py
+++ Code/Validation/testScript.py
@@ -6,20 +6,20 @@ print "Running on differing PCA components..."
 for weight in weights:
     command = ""
     for comp in xrange(20, 201, 20):
-        command = 'python Validation.py -k 70 -c ' + str(comp) + ' -w ' + weight
+        command = 'python Validation.py -k 100 -c ' + str(comp) + ' -w ' + weight
         print "Running " + command + '...',
         stdout.flush()
         system(command + ">> results.csv")
         print "finished"
 
-print "Running on differing knnKs..."
-for weight in weights:
-    command = ""
-    for knnk in xrange(10, 101, 10):
-        command = 'python Validation.py -k '+ str(knnk) + ' -c 100 -w ' + weight
-        print "Running " + command + '...',
-        stdout.flush()
-        system(command + ">> results.csv")
-        print "finished"
+# print "Running on differing knnKs..."
+# for weight in weights:
+#     command = ""
+#     for knnk in xrange(10, 151, 10):
+#         command = 'python Validation.py -k '+ str(knnk) + ' -c 100 -w ' + weight
+#         print "Running " + command + '...',
+#         stdout.flush()
+#         system(command + ">> results.csv")
+#         print "finished"
 
 print "Results saved to results.csv"
