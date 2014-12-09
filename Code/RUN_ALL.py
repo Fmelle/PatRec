39e1bfcc8fbd5a62e09d66fac0d8f5670f6cc3b5
Fmelle <fridtjof.melle@gmail.com>
2014-12-09 16:18:49 -0500

    Added demo users to README

--- Code/RUN_ALL.py
+++ Code/RUN_ALL.py
@@ -2,7 +2,7 @@
 
 import os
 
-usr = 'Pv7DGHzZ-uqIUdOsqPpsVg'
+usr = 'ZrUZtLgLOn15v2NIZl-oKQ'
 usrFile = '../ConvertedCSV/user_feature_matrix.csv'
 reviewFile = '../ConvertedCSV/user_restaurant_review_mapping_ext.csv'
 os.system(' '.join(['python','YelpRecommendation.py', usr, usrFile, reviewFile]))
